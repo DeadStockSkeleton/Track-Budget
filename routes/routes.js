3877b4f642a0c777b3b590da232456dd70e22ee1
Iyana Medlock <iyanamedlock@gmail.com>
2021-05-20 00:16:32 -0500

    service worker added

--- routes/routes.js
+++ routes/routes.js
@@ -14,11 +14,11 @@ router.post('/budget/transaction', async (req, res) => {
 })
 
 router.get('/budget/transaction', async (req, res) => {
-  try{
-   await Transaction.find({}).sort({date: -1}).then(data => {
+  
+   await Transaction.find().sort({date: -1}).then(data => {
       res.status(200).json(data);
     })
-  }catch(err){
+  if(err){
     res.status(404).json(err);
   }
 })
